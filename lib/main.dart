d9fedddce93cce9e055fac6205a41b703592edf1
Farhan <farhan96.hm@gmail.com>
2021-11-30 16:51:17 +0600

    Tutorial 08 - List View

--- lib/main.dart
+++ lib/main.dart
@@ -12,71 +12,62 @@ void main(){
 class HomePage extends StatelessWidget{
   final _longText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
   final _shortText = "This is nothing just a short text";
+
+  List people = [
+    {"name":"Farhan","email":"farhan@gmail.com"},
+    {"name":"Zakaria","email":"zakaria@gmail.com"},
+    {"name":"Ariana","email":"ariana@gmail.com"},
+    {"name":"Grande","email":"farhan@gmail.com"},
+    {"name":"Tom","email":"tom@gmail.com"},
+    {"name":"Jerry","email":"jerry@gmail.com"},
+    {"name":"David","email":"david@gmail.com"},
+    {"name":"Shopia","email":"shopia@gmail.com"},
+    {"name":"warner","email":"warner@gmail.com"},
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Home")),
         body: Column(
           children: [
-            Stack(
-              children: [
-                Image.asset('assets/images/straw-berry.jpg'),
-                Positioned(
-                  bottom: 40,
-                    left: 30,
-                    child: Text("This is Straw Berry",textAlign: TextAlign.center,style: TextStyle(fontSize: 20,color: Colors.yellow),)
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text("Features",style: TextStyle(fontSize: 20,color: Colors.red)),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                  ),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                  ),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30,),
-            Row(children: [
-              _rowCell(0xFFD50000),
-              _rowCell(0xFFB71C1C),
-              _rowCell(0xFFDD2C00),
-            ],),
+            Container(
+              padding: EdgeInsets.all(35),
+              height: 500,
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  _cell(),
+                  Divider(),
+                  _cell(),
+                  Divider(),
+                  _cell(),
+                  Divider(),
+                  _cell(),
+                  Divider(),
+                  _cell(),
+                ],
+              ),
+            )
           ],
         ),
     );
   }
 
-  Widget _rowCell(int _color){
-    return Expanded(child: Container(
-        height: 50,
-        decoration: BoxDecoration(color: Color(_color)),
-      child: Icon(Icons.link),
-    ));
+  Widget _cell(){
+    return Row(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(color: Colors.teal),
+          child: Icon(Icons.lightbulb_outline,color: Colors.white,),
+        ),
+        SizedBox(
+          width: 10,
+        )
+      ],
+    );
   }
 }
 
